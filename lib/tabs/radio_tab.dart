34dcb79697d8c9c5a21066f8dbffb550dd423eb4
Ahmed Aly <ahmed.3lyyyy4@gmail.com>
2024-02-10 13:08:05 +0200

    radio tab is finished

--- lib/tabs/radio_tab.dart
+++ lib/tabs/radio_tab.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:islami/app_theme.dart';
 
 class RadioTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset(
+          'assets/images/radio_image.png',
+        ),
+        Text(
+            'إذاعة القران الكريم',
+          style: AppTheme.lightTheme.textTheme.titleMedium,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.skip_previous),
+            Icon(Icons.play_arrow),
+            Icon(Icons.skip_next),
+          ],
+        ),
+      ],
+    );
   }
 }
